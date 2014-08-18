fc72742f9b0596c6e775598ed21ba471b95fa8a0
MengChenglin <rdmclin2@163.com>
2014-08-18 11:00:42 +0800

    add dreamlist in dreamlistfragment

--- gen/com/mcl/tenthousandhours/R.java
+++ gen/com/mcl/tenthousandhours/R.java
@@ -1091,7 +1091,7 @@ containing a value of this type.
         public static final int action_menu_divider=0x7f050016;
         public static final int action_menu_presenter=0x7f050017;
         public static final int action_mode_close_button=0x7f050024;
-        public static final int action_settings=0x7f05003d;
+        public static final int action_settings=0x7f05003f;
         public static final int activity_chooser_view_content=0x7f050025;
         public static final int always=0x7f05000b;
         public static final int beginning=0x7f050011;
@@ -1100,6 +1100,8 @@ containing a value of this type.
         public static final int default_activity_button=0x7f050028;
         public static final int dialog=0x7f05000e;
         public static final int disableHome=0x7f050008;
+        public static final int dream=0x7f05003e;
+        public static final int dreamlist=0x7f05003d;
         public static final int dropdown=0x7f05000f;
         public static final int edit_query=0x7f050030;
         public static final int end=0x7f050013;
@@ -1195,9 +1197,10 @@ containing a value of this type.
         public static final int abc_simple_decor=0x7f030017;
         public static final int activity_main=0x7f030018;
         public static final int dreamlist_fragment=0x7f030019;
-        public static final int share_fragment=0x7f03001a;
-        public static final int statistics_fragment=0x7f03001b;
-        public static final int support_simple_spinner_dropdown_item=0x7f03001c;
+        public static final int dreamlist_item=0x7f03001a;
+        public static final int share_fragment=0x7f03001b;
+        public static final int statistics_fragment=0x7f03001c;
+        public static final int support_simple_spinner_dropdown_item=0x7f03001d;
     }
     public static final class menu {
         public static final int main=0x7f0c0000;
--- src/com/mcl/tenthousandhours/DreamListFragment.java
+++ src/com/mcl/tenthousandhours/DreamListFragment.java
@@ -1,22 +1,39 @@
 package com.mcl.tenthousandhours;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.ListFragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
 
 public class DreamListFragment extends Fragment {  
-	  
+	private String [] dreams = new String[]{"Ð´×÷","Á·×Ö","±à³Ì","ÒôÀÖ¼øÉÍ"};
     @Override  
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {  
-        return inflateAndSetupView(inflater, container, savedInstanceState, R.layout.dreamlist_fragment);      
+        return inflateAndSetupView(inflater, container, savedInstanceState, R.layout.dreamlist_fragment);  
     }  
       
     private View inflateAndSetupView(LayoutInflater inflater, ViewGroup container,   
             Bundle savedInstanceState, int layoutResourceId) {  
-        View layout = inflater.inflate(layoutResourceId, container, false);  
-          
-        return layout;  
+    	List<Map<String, Object>> listItems = new ArrayList<Map<String,Object>>();
+    	for(int i = 0;i< dreams.length;i++)
+		{
+			Map<String, Object> listItem = new HashMap<String,Object>();
+			listItem.put("dream", dreams[i]);
+			listItems.add(listItem);
+		}
+        View dreamView = inflater.inflate(layoutResourceId, container, false);  
+        SimpleAdapter simpleAdapter = new SimpleAdapter(dreamView.getContext(), listItems,R.layout.dreamlist_item, new String[]{"dream"}, new int[]{R.id.dream});
+		ListView listView = (ListView)dreamView.findViewById(R.id.dreamlist);
+		listView.setAdapter(simpleAdapter);
+        return dreamView;  
     }   
 }  
